2bc42fdeb485e0f5bba70d30408b5f87e0aaab74
Adam377 <adamhaigh185@yahoo.co.uk>
2026-08-29 16:53:16 +0100

    changed function to not have current time passed in

--- GetDelayMinutes.lua
+++ GetDelayMinutes.lua
@@ -1,8 +1,10 @@
 -- returns player delay rounded down to the nearest minute from scheduled time of arrival for timetable instruction. Intended to make scenario look fancy.
 
-function GetDelayMinutes(bookedArrivalTime, actualArrivalTime)
+function GetDelayMinutes(bookedArrivalTime)
 	-- booked time formatted "HH:MM"
     bookedArrivalTimeSeconds = (string.sub(bookedArrivalTime, 1, 2) * 3600) + (string.sub(bookedArrivalTime, 4, 5) * 60)
+
+	actualArrivalTime = SysCall("ScenarioManager:GetTimeOfDay")
 
 	return tostring(Math.floor((actualArrivalTime - bookedArrivalTimeSeconds) / 60))
 end
